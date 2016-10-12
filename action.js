d070ee098d286fc81c040cf734b2bd38be0d71c6
natalia <frymark89>
2016-10-12 21:42:29 +0200

    Small fixes

--- action.js
+++ action.js
@@ -1,114 +1,43 @@
 $(document).ready(function () {
+
     $('#mainWeb').hide();
     $('.moveButton a').click(function () {
         $('#mainWeb').show();
         $('#intro').hide();
-    })
+    });
 
 
-});
-
-// $(document).ready(function () {
-//     $('.picture, .greetingTittle').hide();
-//     $('.moveButton a').mouseover(function () {
-//         $('.picture,.greetingTittle').show();
-//         $('#name').hide();
-//     });
-//     $('.moveButton a').mouseout(function () {
-//         $('.picture, .greetingTittle').hide();
-//         $('#name').show();
-//
-//     })
-// });
- $(document).ready(function () {
-    $('.notLighted').mouseover(function(){
-        $('.menu .enlight').removeClass('enlight');
-        $(this).addClass('enlight');
-        });
-     $('.enlighted').mouseout(function(){
-         $('.menu .enlight').removeClass('enlight');
-         // $(this).addClass('.notLighted');
-     });
-     $()
-
-});
-
-
-$(document).ready(function () {
     $('.notLighted').click(function () {
         $('.menu .enlight').removeClass('enlight');
         $(this).addClass('enlight');
+    });
 
-    })
-});
 
-// $(document).ready(function () {
-//     $('.uniPicture').hide();
-//     $('#floorball').hide();
-//
-//
-//     $('.linkTofloorball').click(function () {
-//         console.log('click pokazujacy');
-//         $('#floorball').show();
-//         $('.uniPicture').show();
-//         $('.linkTofloorball').addClass('hideButton');
-//         $('.linkTofloorball, .hideButton').removeClass('linkTofloorball');
-//
-//         $('.hideButton').click(function () {
-//             $('#floorball').hide();
-//             $('.uniPicture').hide();
-//             $('.hideButton').addClass('linkTofloorball');
-//             $('.hideButton, .linkTofloorball').removeClass('hideButton');
-//             $(this).off();
-//         });
-//     });
-//
-// });
+    $(window).scroll(function () {
+        $(".menu").addClass('darkMenuOnScroll');
+    });
 
-$(document).ready(function () {
+
     $('.floorBallArticle').hide();
 
     $('.linkTofloorball').click(function () {
         $('.floorBallArticle').toggle();
 
     });
-});
 
 
-$(document).ready(function () {
     $('.footOrienteeringArticle').hide();
 
     $('.linkToOrienteering').click(function () {
         $('.footOrienteeringArticle').toggle();
 
     });
-});
-
-// $(document).ready(function(){
-//     $('.pages > div').hide();
-//     $('.menu ul li a').click(function(){
-//         $('#' + $(this).attr('id') + 'Container').show();
-//     });
-//     // $('#menu').
-// });
 
 
-/*Menu Hambureger*/
-
-$(document).ready(function(){
-    $('#nav-icon1').click(function(){
-        $(this).toggleClass('open');
+    $('.sidenavLink').click(function () {
+        closeNav();
     });
-});
-
-$(document).ready(function(){
-    $('.sideBar').hide();
-    $('#nav-icon1').click(function(){
-        $('.sideBar').slideToggle('slow');
-    })
-});
-
-
+}); 
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
